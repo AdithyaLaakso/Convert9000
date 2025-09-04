29683ca24ec72297643d7d023d5f58604b573e7b
AdithyaLaakso <adithya.laakso@gmail.com>
2025-09-04 09:15:49 -0400

    fixed bug due to syntax error

--- convert.zsh
+++ convert.zsh
@@ -2,8 +2,8 @@ function conversion_not_available {
 	input_extension=$1
 	output_extension=$2
 	echo "
-		Conversion from $3 to $4 is not available yet. \n
-		If this is a feasible conversion that would be useful to you, file an issue on github. \n "
+		Conversion from $1 to $2 is not available yet.
+		If this is a feasible conversion that would be useful to you, file an issue on github. "
 	exit 2
 }
 
@@ -18,11 +18,11 @@ function get_extension {
 
 function get_extension_type {
 	case $1 in
-		bibtex| biblatex| bits| commonmark| commonmark_x| creole| csljson| csv| tsv| \
-		djot| docbook| docx| dokuwiki| endnotexml| epub| fb2| gfm| haddock| html| \
-		ipynb| jats| jira| json| latex| markdown| markdown_mmd| markdown_phpextra| \
-		markdown_strict| mediawiki| man| mdoc| muse| native| odt| opml| org| pod| \
-		ris| rtf| rst| t2t| textile| tikiwiki| twiki| typst| vimwiki)
+		bibtex|biblatex|bits|commonmark|commonmark_x|creole|csljson|csv|tsv|\
+		djot|docbook|docx|dokuwiki|endnotexml|epub|fb2|gfm|haddock|html|\
+		ipynb|jats|jira|json|latex|markdown|markdown_mmd|markdown_phpextra|\
+		markdown_strict|mediawiki|man|mdoc|muse|native|odt|opml|org|pod|\
+		ris|rtf|rst|t2t|textile|tikiwiki|twiki|typst|vimwiki)
 			echo "text"
 			;;
 		png|jpg|jpeg|tiff|heic|bmp|webp|tif)
@@ -60,18 +60,21 @@ fi
 input_extension="$(get_extension "$input_file")"
 output_extension="$(get_extension "$output_file")"
 
-input_type=get_extension_type $input_extension
-output_type=get_extension_type $output_extension
+input_type="$(get_extension_type "$input_extension")"
+output_type="$(get_extension_type "$output_extension")"
 
-if [[ input_type == "None" ] || [ output_type == "None" ]] ; then
+echo "$input_type"
+echo "$output_type"
+
+if [[ $input_type == "None" || $output_type == "None" ]] ; then
 	conversion_not_available $input_extension $output_extension
-elif [[ input_type == "text" ] && [ output_type == "text" ]] ; then
+elif [[ $input_type == "text" && $output_type == "text" ]] ; then
 	pandoc -o $output_file $input_file || missing_program "pandoc"
-elif [[ input_type == "image" ] && [ output_type == "image" ]] ; then
-	magick convert $input_file $output_file || missing_program "magick"
-elif [[ input_type == "audio" ] && [ output_type == "audio" ]] ; then
+elif [[ $input_type == "image" && $output_type == "image" ]] ; then
+	magick $input_file $output_file || missing_program "magick"
+elif [[ $input_type == "audio" && $output_type == "audio" ]] ; then
 	ffmpeg -i $input_file -acodec libmp3lame $output_file || missing_program "ffmpeg"
-elif [[ input_type == "video" ] && [ output_type == "video" ]] ; then
+elif [[ $input_type == "video" && $output_type == "video" ]] ; then
 	ffmpeg -i $input_file -acodec libmp3lame $output_file || missing_program "ffmpeg"
 else
 	conversion_not_available $input_extension $output_extension
